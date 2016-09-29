eb22f9ce309e0a220c567df2ccb958eaa9392aec
Richard Rondan <rondanrichard@gmail.com>
2016-09-29 02:24:32 -0500

    CBK: Primer Activity - Login :V

--- app/src/main/java/com/papus/los/carbookapp/activities/LoginActivity.java
+++ app/src/main/java/com/papus/los/carbookapp/activities/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.papus.los.carbookapp;
+package com.papus.los.carbookapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.papus.los.carbookapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
