1d08ef062bb73cf191ba2fd1769d976be937ff5f
Richard Rondan <rondanrichard@gmail.com>
2016-09-28 23:22:43 -0500

    Initial commit #FULL20 #LosPapus

---
+++ app/src/androidTest/java/com/papus/los/carbookapp/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.papus.los.carbookapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.papus.los.carbookapp", appContext.getPackageName());
+    }
+}
